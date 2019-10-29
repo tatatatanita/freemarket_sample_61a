a0bb0aff85d106fec4f258b5b09baf8c558d5bb3
sho40 <kanazawa.sho40@gmail.com>
2019-10-29 17:27:20 +0900

    definitiion new/create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,23 @@ class ProductsController < ApplicationController
   def index
     @product = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    # raise.params.inspect
+    @product = Product.create(product_params)
+    if @product.save
+      redirect_to root_path, notice: '出品しました'
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:title, :image, :text, :price)
+  end
 end

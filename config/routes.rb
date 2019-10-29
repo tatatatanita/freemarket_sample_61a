507563b12faa453ea4bee43368183b9b384425bd
tatatatanita <y_shibata_nagoya@yahoo.co.jp>
2019-10-29 16:56:45 +0900

    product_show_page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:edit, :update]
-  resources :products, only: [:index]
-  # 仮でonly: indexにしてます
+  resources :products, only: [:index, :show]
+  # 仮でonly: indexにしてます,showも付け足しました。
 end

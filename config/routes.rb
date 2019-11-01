75839e517345c5460cb183f983ff05333f0f743e
tsubasaozawa <tsubasaozawa@users.noreply.github.com>
2019-11-01 13:00:01 +0900

    微調整10

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # root to: 'products#index'
+  root to: 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :edit, :update]
   resources :products, only: [:index, :new, :create, :destroy, :show]

d13c21606f10572f49ca321909aa83481b95e7d5
tsubasaozawa <tsubasaozawa@users.noreply.github.com>
2019-11-01 11:57:01 +0900

    微修正8

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,34 +1,34 @@
 class ProductsController < ApplicationController
   def index
-    # @product = Product.all
+    @product = Product.all
   end
 
 
-  # def new
-  #   @product = Product.new
-  # end
+  def new
+    @product = Product.new
+  end
 
-  # def create
-  #   # raise.params.inspect
-  #   @product = Product.create(product_params)
-  #   if @product.save
-  #     redirect_to root_path, notice: '出品しました'
-  #   end
-  # end
+  def create
+    # raise.params.inspect
+    @product = Product.create(product_params)
+    if @product.save
+      redirect_to root_path, notice: '出品しました'
+    end
+  end
 
-  # def destroy
-  #   @product == Product.find(params[:id])
-  #     if @product.user_id == current_user.id
-  #       @product.destroy
-  #       # redirect_to 'show_exhibit' 実装後コメントアウト外す
-  #     end
-  # end 
+  def destroy
+    @product == Product.find(params[:id])
+      if @product.user_id == current_user.id
+        @product.destroy
+        # redirect_to 'show_exhibit' 実装後コメントアウト外す
+      end
+  end 
 
  
   
-  # def show
-  #   @product = Product.find(params[:id])
-  # end
+  def show
+    @product = Product.find(params[:id])
+  end
   
   private
   def product_params

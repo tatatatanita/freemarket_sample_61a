eb646a740eb76ea00f9cf887815f0f705f24a689
sho40 <kanazawa.sho40@gmail.com>
2019-11-01 18:47:10 +0900

    e

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,3 @@ class User < ApplicationRecord
   has_many :buyed_products, foreign_key: "buyer_id", class_name: "Product"
   has_many :saling_products, -> { where("buyer_id is NULL") }, foreign_key: "saler_id", class_name: "Product"
   has_many :sold_products, -> { where("buyer_id is not NULL") }, foreign_key: "saler_id", class_name: "Product"
-
-

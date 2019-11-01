832683df1603708ac5c2ca8d0a4898d922bdbc02
sho40 <kanazawa.sho40@gmail.com>
2019-11-01 15:30:50 +0900

    cretae db

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+         has_many :buyed_products, foreign_key: "buyer_id", class_name: "Product"
+         has_many :saling_products, -> { where("buyer_id is NULL") }, foreign_key: "saler_id", class_name: "Product"
+         has_many :sold_products, -> { where("buyer_id is not NULL") }, foreign_key: "saler_id", class_name: "Product"
+end 

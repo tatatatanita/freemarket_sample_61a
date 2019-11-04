6a437869b3df67dc39f7b48ce7d073f1e7e58829
tsubasaozawa <tsubasaozawa@users.noreply.github.com>
2019-11-04 21:09:33 +0900

    デプロイ用微調整

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,9 +33,4 @@ namespace :deploy do
   end
 end
 
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-}
+set :linked_files, fetch(:linked_files, []).push("config/master.key")

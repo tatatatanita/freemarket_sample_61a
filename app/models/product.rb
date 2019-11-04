5cab67b752610fb6919123f13bb9fddec8f36dfc
sho40 <kanazawa.sho40@gmail.com>
2019-11-04 20:23:28 +0900

    update

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   
   # belongs_to :category　カテゴリー実装じにコメントアウト外す
   
-  belongs_to :saler, class_name: "User"
+  belongs_to :saler, class_name: "User", optional: true
   belongs_to :buyer, class_name: "User", optional: true
 
   has_many :images, inverse_of: :product, dependent: :destroy

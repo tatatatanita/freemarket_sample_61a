a4a15de9e9dd05d84e388cfb4061d73d448ade45
tsubasaozawa <tsubasaozawa@users.noreply.github.com>
2019-11-04 12:47:41 +0900

    uploder.rbの修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,8 +6,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   if Rails.env.development?
     storage :file
-  elsif Rails.env.test?
-    storage :file
   else
     storage :fog
   end

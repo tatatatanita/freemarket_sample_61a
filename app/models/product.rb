26d0334e23992ed6b56446484a3d68ab84552dd9
sho40 <kanazawa.sho40@gmail.com>
2019-11-04 14:12:16 +0900

    association

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,11 @@
 class Product < ApplicationRecord
-  mount_uploader :image, ImageUploader
   
   belongs_to :user, foreign_key: 'user_id'
   belongs_to :category
   
   belongs_to :saler, class_name: "User"
   belongs_to :buyer, class_name: "User"
+
+  has_many :images, inverse_of: :product, dependent: :destroy
+  accepts_nested_attributes_for :images
 end

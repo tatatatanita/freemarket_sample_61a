6d368dbc80cfc7e20649b1930ec6f5dddbc2b972
sho40 <kanazawa.sho40@gmail.com>
2019-11-04 16:51:45 +0900

    buyer add optional:true

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,10 @@
 class Product < ApplicationRecord
   
-  belongs_to :user, foreign_key: 'user_id'
-  belongs_to :category
+  # belongs_to :user, foreign_key: 'user_id'
+  # belongs_to :category
   
   belongs_to :saler, class_name: "User"
-  belongs_to :buyer, class_name: "User"
+  belongs_to :buyer, class_name: "User", optional: true
 
   has_many :images, inverse_of: :product, dependent: :destroy
   accepts_nested_attributes_for :images

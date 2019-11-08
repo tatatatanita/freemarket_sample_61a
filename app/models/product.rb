f737e541da4e2a4589365cbd0ebbd3fddee1d9b5
tatatatanita <y_shibata_nagoya@yahoo.co.jp>
2019-11-08 14:49:43 +0900

    exhibit_product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
   # belongs_to :category カテゴリー実装じにコメントアウト外す
   
   belongs_to :saler, class_name: "User", optional: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_082205) do
+ActiveRecord::Schema.define(version: 2019_11_07_054015) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -70,18 +70,15 @@ ActiveRecord::Schema.define(version: 2019_11_05_082205) do
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "image", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "title", null: false
     t.text "text"
     t.integer "price", null: false
-    t.bigint "user_id_id"
     t.bigint "user_id"
     t.integer "saler_id"
     t.integer "buyer_id"
     t.index ["user_id"], name: "index_products_on_user_id"
-    t.index ["user_id_id"], name: "index_products_on_user_id_id"
   end
 
   create_table "root_areas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|

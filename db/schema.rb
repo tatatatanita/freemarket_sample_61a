5ba0a1b24ebed8c460cdf3cf43de8f24e6f10a97
tatatatanita <y_shibata_nagoya@yahoo.co.jp>
2019-11-09 12:50:47 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_08_044505) do
+ActiveRecord::Schema.define(version: 2019_11_07_111247) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2019_11_08_044505) do
     t.string "first_name_kana", null: false
     t.string "last_name_kana", null: false
     t.integer "postal_code", null: false
-    t.integer "prefectures", null: false
+    t.integer "prefectures", default: 0, null: false
     t.string "city", null: false
     t.string "address", null: false
     t.string "building"
@@ -70,7 +70,6 @@ ActiveRecord::Schema.define(version: 2019_11_08_044505) do
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "image", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "title", null: false
@@ -108,7 +107,6 @@ ActiveRecord::Schema.define(version: 2019_11_08_044505) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "birthday"
     t.integer "certification_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

fdf075d6b791579a68e763034d0cc39c609ff908
tsubasaozawa <tsubasaozawa@users.noreply.github.com>
2019-11-21 13:04:23 +0900

    微修正（電話番号のデータ型変更）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_030147) do
+ActiveRecord::Schema.define(version: 2019_11_21_040010) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -107,7 +107,7 @@ ActiveRecord::Schema.define(version: 2019_11_20_030147) do
     t.string "first_name_kana", null: false
     t.date "birthday", null: false
     t.integer "sex"
-    t.integer "tel_number", null: false
+    t.string "tel_number", null: false
     t.text "profile_text"
     t.text "profile_image"
     t.string "email", default: "", null: false
